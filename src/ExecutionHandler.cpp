bc5846894248e821f82dc1972098f0e8cfee5ccc
Ian <teaqllabs@gmail.com>
2021-10-30 12:31:23 -0400

    Aded treedump.

--- src/ExecutionHandler.cpp
+++ src/ExecutionHandler.cpp
@@ -4,6 +4,13 @@
 void execute(std::map<std::string, std::string> treestream) {
     std::map<std::string, short int> intVars;
 
+    #ifdef TREE_DUMP
+    typedef std::map<std::string, std::string> ss_map;
+    for (ss_map::const_iterator it = treestream.begin(); it != treestream.end(); ++it) {
+        std::cout << "[" << it -> first << ": " << it -> second << "]" << std::endl;
+    }
+    #endif
+
     if (treestream.count("CALLED")) {
         if (treestream["CALLED"] == "OUT") {
             std::cout << treestream["VALUE"] << std::endl;
